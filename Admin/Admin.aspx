1e88cb71a08a320767daabd9d9409c2545781a8d
Luka Stefanovic <lukastef42@gmail.com>
2024-04-13 18:47:19 +0200

    zapocet rad za admin stranicu

--- Admin/Admin.aspx
+++ Admin/Admin.aspx
@@ -1,3 +1,38 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Admin.aspx.cs" Inherits="Projekat.Admin.Admin" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <h1>Database</h1>
+    <br />
+    <h5>View, edit, delete or add a new entry into the database</h5>
+    <br />
+    Load data for: <asp:DropDownList ID="ddlSearch" runat="server" OnSelectedIndexChanged="ddlSearch_SelectedIndexChanged" AutoPostBack="true" CssClass="dropdown">
+        <asp:ListItem Value="b">Band</asp:ListItem>
+        <asp:ListItem Value="a">Album</asp:ListItem>
+        <asp:ListItem Value="s">Song</asp:ListItem>
+    </asp:DropDownList><br />
+    <asp:Panel ID="panelBand" runat="server">
+        <asp:Label ID="lblIdB" runat="server" Text="ID:"></asp:Label> <asp:TextBox ID="tbId" runat="server" CssClass="form-control" Enabled="false"></asp:TextBox>
+        <asp:Label ID="lblNameB" runat="server" Text="Name:"></asp:Label> <asp:TextBox ID="tbName" runat="server" CssClass="form-control"></asp:TextBox>
+        <asp:Label ID="lblLogoB" runat="server" Text="Logo (file path):"></asp:Label> <asp:TextBox ID="tbLogo" runat="server" CssClass="form-control"></asp:TextBox>
+        <asp:Label ID="lblDateB" runat="server" Text="Date:" ></asp:Label> <asp:TextBox ID="tbDate" runat="server" CssClass="form-control"></asp:TextBox>
+        <asp:Label ID="lblSiteB" runat="server" Text="Site:" ></asp:Label> <asp:TextBox ID="tbSite" runat="server" CssClass="form-control"></asp:TextBox>
+
+
+    </asp:Panel>
+    <asp:Panel ID="panelAlbum" runat="server" Visible="false">
+        <asp:Label ID="lblIdA" runat="server" Text="ID:"></asp:Label> <asp:TextBox ID="tbIdA" runat="server" CssClass="form-control" Enabled="false"></asp:TextBox>
+        <asp:Label ID="lblNameA" runat="server" Text="Name:"></asp:Label> <asp:TextBox ID="tbnameA" runat="server" CssClass="form-control"></asp:TextBox>
+        <asp:Label ID="lblCover" runat="server" Text="Album cover (file path):"></asp:Label> <asp:TextBox ID="tbCover" runat="server" CssClass="form-control"></asp:TextBox>
+        <asp:Label ID="lblDateA" runat="server" Text="Date:" ></asp:Label> <asp:TextBox ID="tbDateA" runat="server" CssClass="form-control"></asp:TextBox>
+        <asp:Label ID="lblBand" runat="server" Text="Band (ID):" ></asp:Label> <asp:TextBox ID="tbBand" runat="server" CssClass="form-control"></asp:TextBox>
+    </asp:Panel>
+    <asp:Panel ID="panelSong" runat="server" Visible="false">
+        <asp:Label ID="lblIdS" runat="server" Text="ID:"></asp:Label> <asp:TextBox ID="tbIdS" runat="server" CssClass="form-control" Enabled="false"></asp:TextBox>
+        <asp:Label ID="lblNameS" runat="server" Text="Name:"></asp:Label> <asp:TextBox ID="tbNameS" runat="server" CssClass="form-control"></asp:TextBox>
+        <asp:Label ID="lblAlbum" runat="server" Text="Album (ID):"></asp:Label> <asp:TextBox ID="tbAlbum" runat="server" CssClass="form-control"></asp:TextBox>
+        <asp:Label ID="lblLink" runat="server" Text="Youtube link:" ></asp:Label> <asp:TextBox ID="tblink" runat="server" CssClass="form-control"></asp:TextBox>
+        
+    </asp:Panel>
+    <br />
+    <asp:Label ID="lblError" runat="server" Text="Label"></asp:Label>
+    <asp:GridView ID="GridView1" runat="server" AutoGenerateSelectButton="true" OnSelectedIndexChanged="GridView1_SelectedIndexChanged" CssClass="table" SelectedRowStyle-BackColor="LightGray" SelectedRowStyle-Font-Underline="true"></asp:GridView>
 </asp:Content>
--- Admin/Admin.aspx.cs
+++ Admin/Admin.aspx.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity.Core.Common.CommandTrees.ExpressionBuilder;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -11,7 +13,146 @@ namespace Projekat.Admin
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (IsPostBack)
+                return;
+            try
+            {
+                SqlConnection con = new SqlConnection(@"Data Source=localhost\SQLEXPRESS;Initial Catalog=Projekat;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
 
+                string upit = "SELECT * FROM Izvodjac";
+                TraziBiloSta(con, upit);
+            }
+            catch (Exception x)
+            {
+                lblError.Text = "SERVER ERROR";
+                System.Diagnostics.Debug.WriteLine(x.Message);
+                System.Diagnostics.Debug.WriteLine(x.StackTrace);
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+        void TraziBiloSta(SqlConnection con,string upit)
+        {
+            using (con)
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand(upit, con);
+                SqlDataReader dr = cmd.ExecuteReader();
+                GridView1.DataSource = dr;
+                GridView1.DataBind();
+                dr.Close();
+            }
+        }
+        protected void ddlSearch_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                OcistiTextboxoveAlbum();
+                OcistiTextboxoveBand();
+                OcistitextboxoveSong();
+                SqlConnection con = new SqlConnection(@"Data Source=localhost\SQLEXPRESS;Initial Catalog=Projekat;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+                string upit = "";
+                switch (ddlSearch.SelectedValue)
+                {
+                    case "b":
+                        upit = "SELECT * FROM Izvodjac";
+                        panelBand.Visible = true;
+                        panelAlbum.Visible = false;
+                        panelSong.Visible = false;
+                        break;
+                    case "a":
+                        upit = "SELECT * FROM Album";
+                        panelBand.Visible = false;
+                        panelAlbum.Visible = true;
+                        panelSong.Visible = false;
+                        break;
+                    case "s":
+                        upit = "SELECT * FROM Pesma";
+                        panelBand.Visible = false;
+                        panelAlbum.Visible = false;
+                        panelSong.Visible = true;
+                        break;
+                }
+                TraziBiloSta(con, upit);
+            }
+            catch (Exception x)
+            {
+                lblError.Text = "SERVER ERROR";
+                System.Diagnostics.Debug.WriteLine(x.Message);
+                System.Diagnostics.Debug.WriteLine(x.StackTrace);
+            }
+        }
+
+        protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                GridViewRow row = GridView1.SelectedRow;
+                switch (ddlSearch.SelectedValue)
+                {
+                    case "b":
+                        
+                        tbId.Text = row.Cells[1].Text;
+                        tbName.Text = row.Cells[2].Text;
+                        tbLogo.Text = row.Cells[3].Text;
+                        tbDate.Text = row.Cells[4].Text;
+                        tbSite.Text = row.Cells[5].Text;
+
+                        break;
+                    case "a":
+
+                        tbIdA.Text = row.Cells[1].Text;
+                        tbnameA.Text = row.Cells[2].Text;
+                        tbCover.Text = row.Cells[3].Text;
+                        tbDateA.Text = row.Cells[4].Text;
+                        tbBand.Text = row.Cells[5].Text;
+
+                        break;
+                    case "s":
+
+                        tbIdS.Text = row.Cells[1].Text;
+                        tbNameS.Text = row.Cells[2].Text;
+                        tbAlbum.Text = row.Cells[3].Text;
+                        tblink.Text = row.Cells[4].Text;
+
+                        break;
+                }
+            }
+            catch (Exception x)
+            {
+                lblError.Text = "SERVER ERROR";
+                System.Diagnostics.Debug.WriteLine(x.Message);
+                System.Diagnostics.Debug.WriteLine(x.StackTrace);
+            }
+        }
+
+        void OcistiTextboxoveBand()
+        {
+            tbId.Text = "";
+            tbName.Text = "";
+            tbLogo.Text = "";
+            tbDate.Text = "";
+            tbSite.Text = "";
+        }
+
+        void OcistiTextboxoveAlbum()
+        {
+            tbIdA.Text = "";
+            tbnameA.Text = "";
+            tbCover.Text = "";
+            tbDateA.Text = "";
+            tbBand.Text = "";
+        }
+
+        void OcistitextboxoveSong()
+        {
+            tbIdS.Text = "";
+            tbNameS.Text = "";
+            tbAlbum.Text = "";
+            tblink.Text = "";
         }
     }
 }
